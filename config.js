72baafd501773ea4b05545f721f970b2c96a78d6
Mikel <151082342+Mendiak@users.noreply.github.com>
2025-08-04 10:09:17 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// c:/Users/mikel/Desktop/VS Code/new-contemporary-photography/config.js
-
-window.ENV = {
-  // Reemplaza con tu API Key real de Flickr
-  NEXT_PUBLIC_FLICKR_API_KEY: '76214d6f334339bdc73a080ef025339e', 
-
-  // Reemplaza con tu Group ID real de Flickr
-  NEXT_PUBLIC_FLICKR_GROUP_ID: '1023374@N21'
-};
